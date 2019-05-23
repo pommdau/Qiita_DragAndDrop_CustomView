d0bb75588b77f8ddc6f99b949e08c8cbd36cad74
hikeuchi <hikeuchi@morisawa.co.jp>
2019-05-23 11:13:00 +0900

    initial commit

---
+++ DragAndDropSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DragAndDropSample
+//
+//  Created by HIROKI IKEUCHI on 2019/05/23.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
